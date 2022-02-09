d0b22724ec113fb1692b08e5c092547a94003553
Henry Weigand <henryweigand10@gmail.com>
2022-02-09 12:28:02 -0800

    wireframing

--- models/index.js
+++ models/index.js
@@ -1,26 +1,26 @@
-const User = require('./User');
-const Trip = require('./Trip');
-const Destination = require('./Destination');
-const Expenditure = require('./Expenditure');
+const User = require("./User");
+const Trip = require("./Trip");
+const Destination = require("./Destination");
+const Expenditure = require("./Expenditure");
 
 User.hasMany(Trip, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
-  })
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
 
 Trip.hasMany(Destination, {
-    foreignKey: 'tripId',
-    onDelete: 'CASCADE'
-  })
+  foreignKey: "tripId",
+  onDelete: "CASCADE",
+});
 
 Destination.hasMany(Expenditure, {
-    foreignKey: 'destinationId',
-    onDelete: 'CASCADE'
-  })
+  foreignKey: "destinationId",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   User,
   Trip,
   Destination,
-  Expenditure
-}
+  Expenditure,
+};
