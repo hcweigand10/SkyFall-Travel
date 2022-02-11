8b8cf9e16d80c893b0050779e5adbf5e85c8d47d
Takara Truong <truont2@gmail.com>
2022-02-10 22:02:05 -0800

    update destination works

--- controllers/api/destination-routes.js
+++ controllers/api/destination-routes.js
@@ -4,7 +4,38 @@ const withAuth = require('../../utils/auth');
 
 
 // be able to create
+router.post('/', withAuth, async(req, res) => {
+    const body = req.body; 
 
+    try {
+        const newDestination = await Destination.create({...body, User: req.session.user});
+        req.json(newDestination)
+    } catch(err) {
+        console.log(err); 
+        res.status(500).json(err);
+    }
+});
 // be able to update
-
+// use hidden variable
+// route might be off since we are editing on the page itself
+router.put('/:id', async (req, res) => {
+    try{
+        const [affectedRows] = await Destination.update(req.body, {
+            where: {
+                id: req.params.id,
+            }
+        }); 
+        
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch(err) {
+        console.log(err); 
+        res.status(500).json(err);
+    }
+})
 // be able to delete
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const tripRoutes = require('./trip-routes');
 const userRoutes = require('./user-routes');
+const destinationRoutes = require('./destination-routes');
 
+router.use('/destination', destinationRoutes)
 router.use('/user', userRoutes);
 router.use('/trip', tripRoutes);
 
