8da9da7954bb48ed4dfcaafc65b625bdda79c4f1
Takara Truong <truont2@gmail.com>
2022-02-12 02:01:08 -0800

    donut chart for on the trip view page works

---
+++ public/js/graph.js
@@ -0,0 +1,86 @@
+let destinationBudget= document.getElementsByClassName('budget');
+let destinationNames = document.getElementsByClassName('card-header');
+
+console.log(destinationBudget);
+
+const budgetArr = [];
+const destinationArr = [];
+const colorArr = [];
+
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r,g,b];
+}
+
+function randomHexColor() {
+    let [r,g,b] =randomRgbColor();
+
+    let hr = r.toString(16).padStart(2, '0');
+    let hg = g.toString(16).padStart(2, '0');
+    let hb = b.toString(16).padStart(2, '0');
+
+    return "#" + hr + hg + hb;
+}
+let totalBudget = 0;
+
+
+// loop to grab information of each destination name and its budget
+for(let i = 0; i < destinationBudget.length; i++) {
+    budgetArr.push(parseInt(destinationBudget[i].innerHTML))
+    totalBudget += parseInt(destinationBudget[i].innerHTML)
+    destinationArr.push(destinationNames[i].innerHTML);
+    colorArr.push(randomHexColor());
+}
+
+console.log(budgetArr);
+console.log(destinationArr);
+console.log(colorArr)
+console.log(totalBudget);
+
+  const data = {
+    labels: destinationArr,
+    datasets: [{
+      label: 'My First Dataset',
+      data: budgetArr,
+      backgroundColor: colorArr,
+      hoverOffset: 4
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Trip Budget',
+                padding: {
+                    top: 10,
+                    bottom: 30
+                }, 
+                font: {
+                    size: 60
+                }, 
+            }, 
+            subtitle: {
+                display: true,
+                text: `Total Budget: ${totalBudget}`, 
+                font: {
+                    size: 30
+                }, 
+            }
+        }
+    }
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
