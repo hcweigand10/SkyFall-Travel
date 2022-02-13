7d89f3d8d2aeebcfa9f387c58807533dd2d8d626
Takara Truong <truont2@gmail.com>
2022-02-13 14:11:21 -0800

    add navbar

--- public/js/test-graph.js
+++ public/js/test-graph.js
@@ -10,7 +10,7 @@ const labels = [
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My First dataset',
+      label: 'Flight Prices this year',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
       data: [0, 10, 5, 2, 20, 30, 45],
